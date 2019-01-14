07faa2b3f58efd1e72f93e9daf47c1f52b0c6ea1
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2019-01-13 20:14:06 -0500

    Add chapter 6 materials.

---
+++ ch_06/reg_resid_plot.py
@@ -0,0 +1,41 @@
+import itertools
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def reg_resid_plots(data):
+    """
+    Using seaborn, plot the regression and residuals
+    plots side-by-side for every permutation of 2 columns 
+    in the data.
+    
+    Parameters:
+        - data: A pandas DataFrame
+
+    Returns:
+        A matplotlib Figure object.
+    """
+    num_cols = data.shape[1]
+    permutation_count = num_cols * (num_cols - 1)
+
+    fig, ax = plt.subplots(
+        permutation_count, 
+        2, 
+        figsize=(15, 8)
+    )
+
+    for (x, y), axes, color in zip(
+        itertools.permutations(data.columns, 2), 
+        ax,
+        itertools.cycle(['royalblue', 'darkorange'])
+    ):
+        for subplot, func in zip(axes, (sns.regplot, sns.residplot)):
+            func(
+                x=x, 
+                y=y,
+                data=data,
+                ax=subplot,
+                color=color
+            )
+    plt.close()
+    return fig
