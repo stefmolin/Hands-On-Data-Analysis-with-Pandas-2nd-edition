0fc91fff7c2a9d8d047dc7c7e1c780cf5874da24
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2019-03-30 12:56:07 -0400

    Fix ROC curves and clarify the parameter content.

--- ch_09/utils.py
+++ ch_09/utils.py
@@ -48,7 +48,7 @@ def plot_roc(y_test, preds):
 
     Parameters: 
         - y_test: The true values for y
-        - preds: The predicted values for y
+        - preds: The predicted values for y as probabilities
 
     Returns:
         Plotted ROC curve.
