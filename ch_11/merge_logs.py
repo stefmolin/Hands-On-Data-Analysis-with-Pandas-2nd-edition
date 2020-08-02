f2ef2adbd3ae65e3d8ce77d1725c8f828134b7c9
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2020-08-01 23:03:49 -0400

    Updating for new version of pandas.

--- ch_11/merge_logs.py
+++ ch_11/merge_logs.py
@@ -1,3 +1,5 @@
+import os
+import sys
 import pandas as pd
 
 def cat_csvs(format_string_file_pattern, index_col, month_list):
@@ -16,27 +18,31 @@ def cat_csvs(format_string_file_pattern, index_col, month_list):
         pd.read_csv(
             format_string_file_pattern.format(file), index_col=index_col, parse_dates=True
         ) for file in month_list
-    ])
+    ]).sort_index()
 
 if __name__ == '__main__':
+    # make sure we write the files to the proper directory no matter where we called the script from
+    directory = os.path.dirname(os.path.realpath(sys.argv[0]))
+    os.chdir(directory)
+
     logs_2018 = cat_csvs(
-        'logs/{}_2018.csv', 'datetime', 
+        'logs/{}_2018.csv', 'datetime',
         ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
     )
-    logs_2018['2018'].sort_index().to_csv('logs/logs_2018.csv') # sometimes the simulation overshoots the end date
+    logs_2018['2018'].to_csv('logs/logs_2018.csv') # sometimes the simulation overshoots the end date
 
-    logs_2019 = pd.concat([cat_csvs('logs/{}_2019.csv', 'datetime', ['jan', 'feb', 'mar']), logs_2018.get('2019')])
+    logs_2019 = pd.concat([cat_csvs('logs/{}_2019.csv', 'datetime', ['jan', 'feb', 'mar']), logs_2018.get('2019')]).sort_index()
     logs_2019['2019-Q1'].to_csv('logs/logs_2019.csv') # sometimes the simulation overshoots the end date
 
     hackers_2018 = cat_csvs(
         'logs/hackers_{}_2018.csv', 'start',
         ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
     )
-    hackers_2018['2018'].sort_index().to_csv('logs/hackers_2018.csv')
+    hackers_2018['2018'].to_csv('logs/hackers_2018.csv')
 
     hackers_2019 = pd.concat([
         cat_csvs('logs/hackers_{}_2019.csv', 'start', ['jan', 'feb', 'mar']), hackers_2018.get('2019')
-    ])
-    hackers_2019['2019-Q1'].sort_index().to_csv('logs/hackers_2019.csv')
+    ]).sort_index()
+    hackers_2019['2019-Q1'].to_csv('logs/hackers_2019.csv')
 
     print('All done!')
