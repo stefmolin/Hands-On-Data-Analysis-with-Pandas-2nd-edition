15719690d7d0cb896e469a263156e37eae0bc445
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2020-10-10 18:11:57 -0400

    Update for new seaborn.

--- ch_06/reg_resid_plot.py
+++ ch_06/reg_resid_plot.py
@@ -5,15 +5,14 @@ import seaborn as sns
 
 def reg_resid_plots(data):
     """
-    Using seaborn, plot the regression and residuals
-    plots side-by-side for every permutation of 2 columns 
-    in the data.
+    Using seaborn, plot the regression and residuals plots
+    side-by-side for every permutation of 2 columns in the data.
     
     Parameters:
         - data: A pandas DataFrame
 
     Returns:
-        A matplotlib Figure object.
+        A matplotlib Axes object.
     """
     num_cols = data.shape[1]
     permutation_count = num_cols * (num_cols - 1)
@@ -27,5 +26,4 @@ def reg_resid_plots(data):
     ):
         for subplot, func in zip(axes, (sns.regplot, sns.residplot)):
             func(x=x, y=y, data=data, ax=subplot, color=color)
-    plt.close()
-    return fig
+    return fig.axes
