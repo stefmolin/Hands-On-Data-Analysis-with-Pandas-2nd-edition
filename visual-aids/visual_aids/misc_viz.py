656f85f442ec3327bec5bc7e2c1c50371bbb15d5
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2020-12-07 11:53:24 -0500

    Move viz code for cut() and qcut() buckets to visual_aids

--- visual-aids/visual_aids/misc_viz.py
+++ visual-aids/visual_aids/misc_viz.py
@@ -10,6 +10,46 @@ import seaborn as sns
 from sklearn.cluster import KMeans
 
 
+def low_med_high_bins_viz(data, column, ylabel, title, figsize=(15, 3)):
+    """Visualize the low, medium, and high equal-width bins."""
+    ax = data.plot(y=column, figsize=figsize, color='black', title=title)
+
+    xlims = ax.get_xlim()
+
+    for bin_name, hatch, bounds in zip(
+        ['low', 'med', 'high'],
+        ['///', '', '\\\\\\'],
+        pd.cut(data[column], bins=3).unique().categories.values
+    ):
+        plt.axhspan(bounds.left, bounds.right, alpha=0.2, label=bin_name, hatch=hatch, color='black')
+        plt.annotate(f'  {bin_name}', xy=(xlims[0], (bounds.left + bounds.right) / 2.1), ha='left')
+
+    ax.set(xlabel='', ylabel=ylabel)
+    plt.legend(bbox_to_anchor=(1, 0.75), frameon=False)
+
+    return ax
+
+
+def quartile_bins_viz(data, column, ylabel, title, figsize=(15, 8)):
+    """Visualize quartile bins."""
+    ax = data.plot(y=column, figsize=figsize, color='black', title=title)
+
+    xlims = ax.get_xlim()
+
+    for bin_name, hatch, bounds in zip(
+        [r'$Q_1$', r'$Q_2$', r'$Q_3$', r'$Q_4$'],
+        ['\\\\\\', '', '///', '||||'],
+        pd.qcut(data.volume, q=4).unique().categories.values
+    ):
+        plt.axhspan(bounds.left, bounds.right, alpha=0.2, label=bin_name, hatch=hatch, color='black')
+        plt.annotate(f'  {bin_name}', xy=(xlims[0], (bounds.left + bounds.right) / 2.1), fontsize=11)
+
+    ax.set(xlabel='', ylabel=ylabel)
+    plt.legend(bbox_to_anchor=(1, 0.67), frameon=False, fontsize=14)
+
+    return ax
+
+
 def elliptical_orbit():
     """Draw an example of a planet with an elliptical orbit around its star."""
     fig, axes = plt.subplots(1, 1)
