02998da36bb57fcc2206b324bf1c92b27ce3d38c
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2021-04-19 15:38:10 -0400

    Update docstring

--- ch_04/window_calc.py
+++ ch_04/window_calc.py
@@ -8,15 +8,15 @@ def window_calc(df, func, agg_dict, *args, **kwargs):
     Parameters:
         - df: The `DataFrame` object to run the calculation on.
         - func: The window calculation method that takes `df`
-                as the first argument.
-        - agg_dict: Information to pass to `agg()`, could be a 
-                    dictionary mapping the columns to the aggregation 
-                    function to use, a string name for the function, 
-                    or the function itself.
+          as the first argument.
+        - agg_dict: Information to pass to `agg()`, could be a
+          dictionary mapping the columns to the aggregation
+          function to use, a string name for the function,
+          or the function itself.
         - args: Positional arguments to pass to `func`.
         - kwargs: Keyword arguments to pass to `func`.
     
     Returns:
-        - A new `DataFrame` object.
+        A new `DataFrame` object.
     """
     return df.pipe(func, *args, **kwargs).agg(agg_dict)
