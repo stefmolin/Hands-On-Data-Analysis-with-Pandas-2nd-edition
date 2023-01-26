952f86653e8e646ac468ee6fdbc50befbdfde135
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2023-01-26 16:53:11 -0500

    Rework env check script for use in GitHub Action.

--- ch_01/check_environment.py
+++ ch_01/check_environment.py
@@ -1,24 +1,35 @@
 """Check environment for following along with the text."""
 
-from __future__ import print_function
 from distutils.version import LooseVersion as Version
 import importlib
 import sys
 import re
+
 
 OK = '\x1b[42m[ OK ]\x1b[0m'
 FAIL = '\x1b[41m[FAIL]\x1b[0m'
 
 github_package_pattern = re.compile(r'(?:\/)([\w*\-*]*)(?:\.git)')
 
-def run_checks():
-    """Check that the packages we need are installed and the Python version is high enough."""
+def run_checks(raise_exc=False):
+    """
+    Check that the packages we need are installed and the Python version is good.
+
+    Parameters
+    ----------
+    raise_exc : bool, default ``False``
+        Whether to raise an exception if any of the packages doesn't
+        match the requirements (used for GitHub Action).
+    """
+    failures = []
+
     # check the python version
     print('Using Python in %s:' % sys.prefix)
-    if Version(sys.version) >= '3.7.1':
+    if Version(sys.version) >= '3.7.1' and Version(sys.version) < '3.10.0':
         print(OK, 'Python is version %s\n' % sys.version)
     else:
-        print(FAIL, 'Python version 3.7.1+ is required, but %s is installed.\n' % sys.version)
+        print(FAIL, 'Python version >= 3.7.1 and < 3.10.0 is required, but %s is installed.\n' % sys.version)
+        failures.append('Python')
 
     # read in the requirements
     with open('../requirements.txt', 'r') as file:
@@ -56,14 +67,23 @@ def run_checks():
                     min_version, max_version = req_version
                     if Version(version) < min_version or Version(version) > max_version:
                         print(FAIL, '%s version >= %s and <= %s is required, but %s installed.' % (pkg, min_version, max_version, version))
+                        failures.append(pkg)
                         continue
                 else:
                     if Version(version) != req_version:
                         print(FAIL, '%s version %s is required, but %s installed.' % (pkg, req_version, version))
+                        failures.append(pkg)
                         continue
             print(OK, '%s' % pkg)
         except ImportError:
             print(FAIL, '%s not installed.' % pkg)
+            failures.append(pkg)
+
+    if failures and raise_exc:
+        raise Exception(
+            'Environment failed inspection due to incorrect versions '
+            f'of {len(failures)} item(s): {", ".join(failures)}.'
+        )
 
 if __name__ == '__main__':
-    run_checks()
+    run_checks(raise_exc=True)
